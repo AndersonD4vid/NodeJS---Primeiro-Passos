0c0f9bc19920d60b8da2480effc8a6cbbd0febc8
Anderson David <57516562+AndersonD4vid@users.noreply.github.com>
2023-07-18 19:18:57 -0300

    feat: criando um crud simples

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 const cursos = [
    'NodeJS',
@@ -9,10 +10,49 @@ const cursos = [
    'PostgreSQL'
 ]
 
-server.get('/curso/:index', (req, res) => {
+
+// Create, criando um curso
+server.post('/cursos', (req, res) => {
+   const { name } = req.body;
+   cursos.push(name);
+
+   return res.json(cursos);
+})
+
+// Read, buscando todos os cursos
+server.get('/cursos', (req, res) => {
+   return res.json(cursos);
+})
+
+// Read, buscando um curso
+server.get('/cursos/:index', (req, res) => {
    const { index } = req.params;
 
    return res.json(cursos[index])
 })
+
+// Update, atualizando um curso
+server.put('/cursos/:index', (req, res) => {
+   const { index } = req.params;
+   const { name } = req.body;
+
+   cursos[index] = name;
+
+   return res.json(cursos);
+})
+
+// Delete, deletando um curso
+server.delete('/cursos/:index', (req, res) => {
+   const { index } = req.params;
+
+   cursos.splice(index, 1);
+
+   return res.json({ message: 'Curso deletado' });
+})
+
+
+
+
+
 
 server.listen(3333, () => console.log('Servidor OK'));
