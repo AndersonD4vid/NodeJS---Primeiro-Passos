4979c91578df0d96fa90846fbbea5629183b2adc
Anderson David <57516562+AndersonD4vid@users.noreply.github.com>
2023-07-18 19:32:02 -0300

    feat: criando Middleware global e Middleware de verificação

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 - É um software que permite a execução de códigos JavaScript fora de um navegador web. A principal característica do Node.js é sua arquitetura assíncrona e orientada por eventos.
 E nos permite usar JavaScript na parte do Backend, e não, ele não é uma linguagem.
 
+## O que são Middlewares?
+- São todo tipo de função que estar entre o pedido da requisição e entre o pedido da resposta final para o frontend.
+
 
 # Dependências
 - express: É um framework para Node.js que fornece recursos mínimos para construção de servidores Web. 
--- index.js
+++ index.js
@@ -10,14 +10,31 @@ const cursos = [
    'PostgreSQL'
 ]
 
+// Middleware Global
+// adicionamos o next para seguir o fluxo
+server.use((req, res, next) => {
+   console.log(`URL chamada: ${req.url}`);
+
+   return next();
+})
+
 
 // Create, criando um curso
-server.post('/cursos', (req, res) => {
+server.post('/cursos', checkCurso, (req, res) => {
    const { name } = req.body;
    cursos.push(name);
 
    return res.json(cursos);
 })
+
+// Middleware de verificação
+function checkCurso(req, res, next) {
+   if (!req.body.name) {
+      return res.status(400).json({ error: 'Nome do curso é obrigatório!' });
+   }
+
+   return next();
+}
 
 // Read, buscando todos os cursos
 server.get('/cursos', (req, res) => {
@@ -32,7 +49,7 @@ server.get('/cursos/:index', (req, res) => {
 })
 
 // Update, atualizando um curso
-server.put('/cursos/:index', (req, res) => {
+server.put('/cursos/:index', checkCurso, (req, res) => {
    const { index } = req.params;
    const { name } = req.body;
 
